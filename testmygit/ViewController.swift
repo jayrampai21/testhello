16a599fdf2922a2fe16c85c5cf7ad652688a5b4b
V&V user <vvuser@juventus-mac.local>
2016-12-29 14:53:28 +0530

    Initial Commit

---
+++ testmygit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testmygit
+//
+//  Created by V&V user on 29/12/16.
+//  Copyright © 2016 V&V user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
